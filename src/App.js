b22a780f894f676a578177b1025df661beceaf5d
Shamil <badalloffsh@gmail.com>
2021-03-04 16:58:41 +0100

    Add: HashRouter instead of BrowserRouter. for github pages only

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { HashRouter as Router, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 export function App() {
   return (
-    <Router>
-        <Switch>
-            <Route path="/recent">
-                <p>I will be the recent posts page</p>
-            </Route>
-            <Route path="/sbout">
-                <p>I will be about me page</p>
-            </Route>
-            <Route path="/">
-                <p>I am going to be a Minimalist Life style blog written on react</p>
-            </Route>
-        </Switch>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/">
+          <p>I am going to be a Minimalist Life style blog written on React</p>
+        </Route>
+        <Route path="/recent">
+          <p>I will be the recent posts page</p>
+        </Route>
+        <Route path="/sbout">
+          <p>I will be about me page</p>
+        </Route>
+        {/* {implement not found page} */}
+      </Switch>
     </Router>
   );
 }
